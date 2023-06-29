daad165455c7f829fd777661a4ace9efd8696dbf
Abhinand <abhinandc97@gmail.com>
2023-06-29 19:08:35 +0530

    added get_url function in models

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
 class Category(models.Model):
@@ -7,12 +9,13 @@ class Category(models.Model):
     descripriprion = models.TextField()
     image = models.ImageField(upload_to='category')
 
-
     class Meta:
         ordering=('name',)
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
+    def get_url(self):
+        return reverse('shop:products_by_category',args=[self.slug])
 
     def __str__(self):
         return '{}'.format(self.name)
@@ -31,12 +34,13 @@ class Product(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-
-
     class Meta:
         ordering = ('name',)
         verbose_name = 'product'
         verbose_name_plural = 'products'
+
+    def get_url(self):
+        return reverse('shop:products_by_category',args=[self.slug])
 
     def __str__(self):
         return '{}'.format(self.name)
