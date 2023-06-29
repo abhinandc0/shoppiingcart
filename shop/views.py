c4348e12416a51843063a28963fa001413776b79
Abhinand <abhinandc97@gmail.com>
2023-06-29 19:09:10 +0530

    added view function for all products

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . models import Category, Product
+
 
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def allProdCat(request, c_slug=None):
+    c_page=None
+    products=None
+    if c_slug != None:
+        c_page=get_object_or_404(Category,slug=c_slug)
+        products=Product.objects.all().filter(category=c_page,available=True)
+    else:
+        products=Product.objects.all().filter(available=True)
+
+    print(c_page)
+    print(products)
+    return render(request,"category.html",{'category':c_page, 'products':products})
