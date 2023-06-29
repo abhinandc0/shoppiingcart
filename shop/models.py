84fab4336e092ad81479bf7951cb2f228de0fcdb
Abhinand <abhinandc97@gmail.com>
2023-06-29 12:39:17 +0530

    basic setup with changes to settings, urls of project, and views. Created templates folder and static, made settings for static

---
+++ shop/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=250,unique=True)
+    slug = models.SlugField(max_length=250,unique=True)
+    descripriprion = models.TextField()
+    image = models.ImageField(upload_to='category')
+
+
+    class Meta:
+        ordering=('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+
+    def __str__(self):
+        return '{}'.format(self.name)
+    
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250,unique=True)
+    slug = models.SlugField(max_length=250,unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product',blank=True)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return '{}'.format(self.name)
+    
