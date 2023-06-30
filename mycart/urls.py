ce8411410799ae9bbac450d861194a2412f8f23e
Abhinand <abhinandc97@gmail.com>
2023-06-30 21:33:41 +0530

    cart app created and completed with add item and count cart items, delete and remove item not included

---
+++ mycart/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'mycart'
+
+
+urlpatterns = [
+    path('add/<int:product_id>/',views.add_cart,name='add_cart' ),
+    path('',views.cart_detail,name='cart_detail')
+]
