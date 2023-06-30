eee4f118df800265bbb610d65a9183b628581099
Abhinand <abhinandc97@gmail.com>
2023-06-30 22:04:50 +0530

    added functions for removing item and deleting item from cart

--- mycart/urls.py
+++ mycart/urls.py
@@ -6,5 +6,7 @@ app_name = 'mycart'
 
 urlpatterns = [
     path('add/<int:product_id>/',views.add_cart,name='add_cart' ),
-    path('',views.cart_detail,name='cart_detail')
+    path('',views.cart_detail,name='cart_detail'),
+    path('remove/<int:product_id>/',views.cart_remove,name='cart_remove'),
+    path('full_remove/<int:product_id>/',views.full_remove,name='full_remove'),
 ]
